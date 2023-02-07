2895d3ed6d83e00e929560f36c3a3e77ab96bf22
an-Iceberg <a.g.marosi@bluewin.ch>
2023-02-07 15:01:28 +0100

    Made some progress

--- script.js
+++ script.js
@@ -4,28 +4,28 @@
 
 // By id
 const title = document.getElementById('main-heading');
-console.log(title);
+console.log('Title:', title);
 
 // By class
 const list = document.getElementsByClassName('list-items');
-console.log(list);
+console.log('List:', list);
 
 // By HTML tag
 {
   const list_items = document.getElementsByTagName('li');
-  console.log(list_items);
+  console.log('List:', list_items);
 }
 
 // querySelector()
 {
   const container = document.querySelector('.container');
-  console.log(container);
+  console.log('Container:', container);
 }
 
 // querySelectorAll()
 {
   const list_items = document.querySelectorAll('li');
-  console.log(list_items);
+  console.log('List items:', list_items);
 }
 
 // Changing the style of elements
@@ -37,3 +37,54 @@ for (let index = 0; index < list.length; index++)
 {
   list[index].style.color = 'orange';
 }
+
+{
+  // Creating elements
+  const ul = document.querySelector('ul')
+  const li = document.createElement('li')
+
+  // Adding a class
+  li.setAttribute('id', 'main-heading')
+  li.removeAttribute('id', 'main-heading')
+  li.classList.add('list-items')
+  // li.classList.remove('list-items')
+
+  console.log(li.getAttribute('class'));
+
+  // Adding text
+  li.innerText = 'Some Movie'
+
+  ul.append(li)
+}
+
+// Removing a HTML element
+document.querySelector('#first-list-item').remove()
+
+// Navigating the DOM
+
+// Parent node traversal
+let ul = document.querySelector('ul')
+console.log('ul.parentElement:', ul.parentElement);
+console.log('ul.parentNode:', ul.parentNode);
+
+// Child node traversal
+console.log('ul.childNodes:', ul.childNodes);
+console.log('ul.childElementCount:', ul.childElementCount);
+console.log('ul.firstChild:', ul.firstChild);
+console.log('ul.lastChild:', ul.lastChild);
+console.log('ul.firstElementChild:', ul.firstElementChild);
+console.log('ul.lastElementChild:', ul.lastElementChild);
+
+// ul.childNodes[4].style.backgroundColor = 'red';
+
+console.log('ul.children:', ul.children);
+
+ul.children[1].style.backgroundColor = 'red'
+
+// Sibling node traversal
+console.log('ul.previousSibling:', ul.previousSibling);
+console.log('ul.previousElementSibling:', ul.previousElementSibling);
+console.log('ul.nextSibling:', ul.nextSibling);
+console.log('ul.nextElementSibling:', ul.nextElementSibling);
+
+// Event listeners
